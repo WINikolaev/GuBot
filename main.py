ab92269b7e2d5620d0e0320420c982f722554d80
igni <igni@gurtam.com>
2020-01-04 14:48:33 +0300

    get events from calendar and send msg to telegram

--- main.py
+++ main.py
@@ -1,9 +1,12 @@
 from	__future__ import print_function
 import	telebot
+from	telebot.types import Message
+
 import	datetime
 import	pickle
 import	os.path
 import	json
+import	requests
 
 from	googleapiclient.discovery		import build
 from	google_auth_oauthlib.flow		import InstalledAppFlow
@@ -13,16 +16,22 @@ from	google.auth.transport.requests	import Request
 Discription:
 google api:
 https://developers.google.com/calendar/v3/reference
+https://github.com/gsuitedevs/python-samples/tree/master/calendar/quickstart
+http://wescpy.blogspot.com/2015/09/creating-events-in-google-calendar.html
+https://readthedocs.org/
+https://rtportal.ru/index.php/stati/164-calendar-python-bot
+
+bot:
+https://core.telegram.org/bots/api
+https://github.com/eternnoir/pyTelegramBotAPI
+
 '''
 #TODO: get coordinates
 
-
-'''
 TOKEN = '931079067:AAFedzMljRnB8TlMC8MJSSJ_K3JAfXYw2Fg'
+chat_id = '931079067'
 print(TOKEN)
 bot = telebot.TeleBot(TOKEN)
-'''
-
 # If modifying these scopes, delete the file token.pickle.
 SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
 
@@ -38,21 +47,19 @@ def main():
 			creds.refresh(Request())
 		else:
 			flow = InstalledAppFlow.from_client_secrets_file('credentials.json', SCOPES)
-			print('flow: {0}'.format(flow))
 			creds = flow.run_local_server(port=0)
-			print('creds: {0}'.format(creds))
 			with open('token.pickle', 'wb') as token:
 				pickle.dump(creds, token)
 	
 	service = build('calendar', 'v3', credentials=creds)
+	print('service', service)
 	now = datetime.datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+	print('time now', now)
 	print('Getting the upcoming 10 events')
 	events_result = service.events().list(calendarId='primary', timeMin=now,
 					maxResults=10, singleEvents=True,
 					orderBy='startTime').execute()
-
 	events = events_result.get('items', [])
-	
 	
 	with open('report.json', 'w') as w:
 		json.dump(events, w)
@@ -61,7 +68,16 @@ def main():
 		print('No upcoming events found.')
 	for event in events:
 		start = event['start'].get('dateTime', event['start'].get('date'))
-		print(start, event['summary'])
+		print(start, event['start']['dateTime'])
+		for t in event:
+			print('t: ', event[t])
+	#bot.send_message('849914042', event)
+	#bot.polling()
+
+@bot.message_handler(func=lambda message: True)
+def upper(message: Message):
+	print(message)
+	bot.reply_to(message, 'test-test')
 
 if __name__ == "__main__":
 	main()
